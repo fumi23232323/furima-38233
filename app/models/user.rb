9ed6dfec75724de0904a057c1a3379d4fb56cfc3
fumi23232323 <ftfumift@gmail.com>
2022-10-13 21:13:37 +0900

    Basic認証を導入

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,14 +3,23 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-
-  validates :nickname, presence: true
-  validates :last_name, presence: true
-  validates :first_name, presence: true
-  validates :last_name_kana, presence: true
-  validates :first_name_kana, presence: true
-  validates :birthday, presence: true
-       
   has_many :items  
   has_many :orders
+
+  with_options presence: true do
+  # 半角英数字混合
+  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i.freeze
+  validates_format_of :password, with: PASSWORD_REGEX, message: 'Include both letters and numbers'
+
+  validates :nickname, uniqueness: true
+  validates :email, uniqueness: true
+  # 全角ひらがな カタカナ 漢字
+  validates :last_name, format: { with: /\A[ぁ-んァ-ン一-龥]/ }
+  validates :first_name, format: { with: /\A[ぁ-んァ-ン一-龥]/ } 
+  # 全角カタカナ
+  validates :last_name_kana, format: { with: /\A[ァ-ヶー－]+\z/ }
+  validates :first_name_kana, format: { with: /\A[ァ-ヶー－]+\z/ }
+
+  validates :birthday
+  end
 end
