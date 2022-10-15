f2ae4b9873fd679fac257217cb81e63564ec93df
fumi23232323 <ftfumift@gmail.com>
2022-10-15 22:36:27 +0900

    バリデーションの修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -14,8 +14,8 @@ class User < ApplicationRecord
   validates :nickname, uniqueness: true
   validates :email, uniqueness: true
   # 全角ひらがな カタカナ 漢字
-  validates :last_name, format: { with: /\A[ぁ-んァ-ン一-龥]/ }
-  validates :first_name, format: { with: /\A[ぁ-んァ-ン一-龥]/ }
+  validates :last_name, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]/ }
+  validates :first_name, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]/ }
   # 全角カタカナ
   validates :last_name_kana, format: { with: /\A[ァ-ヶー－]+\z/ }
   validates :first_name_kana, format: { with: /\A[ァ-ヶー－]+\z/ }
