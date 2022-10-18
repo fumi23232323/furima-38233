39ae735ff976dea5ba66b17bf1ed171edc34ed8f
fumi23232323 <ftfumift@gmail.com>
2022-10-18 09:48:55 +0900

    itemsテーブル作成

---
+++ app/models/item.rb
@@ -0,0 +1,15 @@
+class Item < ApplicationRecord
+# has_one_attached :ファイル名
+belongs_to:user
+# has_one:order
+with_options presence: true do
+validates:user 
+validates:item_name
+validates:item_description
+validates:item_category_id
+validates:item_status_id
+validates:delivery_charge_id
+validates:prefecture_id 
+validates:delivery_days_id
+validates:price format: numericality: {only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999, message: 'is invalid'}
+end
---
+++ db/migrate/20221017012608_create_items.rb
@@ -0,0 +1,17 @@
+class CreateItems < ActiveRecord::Migration[6.0]
+  def change
+    create_table :items do |t|
+      t.references :user,               null: false, foreign_key:true
+      t.string     :item_name,          null: false
+      t.text       :item_description,   null: false
+      t.integer    :item_category_id,   null: false
+      t.integer    :item_status_id,     null: false
+      t.integer    :delivery_charge_id, null: false
+      t.integer    :prefecture_id,      null: false
+      t.integer    :delivery_days_id,   null: false
+      t.integer    :price,              null: false
+
+      t.timestamps
+    end
+  end
+end
