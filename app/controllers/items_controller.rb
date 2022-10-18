b2bde67ad6bae04c8573cfd7b021532ae8f3b4ed
fumi23232323 <ftfumift@gmail.com>
2022-10-18 10:18:40 +0900

    画像ファイルの紐付け

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -5,5 +5,9 @@ class ItemsController < ApplicationController
 
   def new
   end
+  private
 
+  def item_params
+    params.require(:item).permit(:item_name, :item_description, :item_category_id, :item_status_id, :delivery_charge_id, :prefecture_id, :delivery_days_id, :price, :image).merge(user_id: current_user.id)
+  end
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,5 +1,5 @@
 class Item < ApplicationRecord
-# has_one_attached :ファイル名
+has_one_attached :image
 belongs_to:user
 # has_one:order
 with_options presence: true do
