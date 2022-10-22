6200f226f04575a8917adbf464be78ee92a6559f
fumi23232323 <ftfumift@gmail.com>
2022-10-22 14:12:51 +0900

    itemモデルのバリデーション修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -22,7 +22,7 @@ validates:item_status_id
 validates:delivery_charge_id
 validates:prefecture_id 
 validates:delivery_days_id
-validates:price, numericality: {only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999}, format: { with: /\A[0-9]+\z/ }
+validates:price, numericality: {only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999}
 end
 
 with_options numericality: { other_than: 0 } do
