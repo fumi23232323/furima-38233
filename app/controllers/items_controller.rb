79efc596da432ec948daa7d898f3377a309a34f2
fumi23232323 <ftfumift@gmail.com>
2022-10-28 23:11:01 +0900

    updateコントローラーの修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -31,12 +31,13 @@ class ItemsController < ApplicationController
   end
 
   def update
-    item = Item.find(params[:id])
-    if item.update(item_params)
+    @item = Item.find(params[:id])
+    if @item.update(item_params)
       redirect_to item_path(item_params)
     else
       render :edit
     end
+  
   end
 
   private
