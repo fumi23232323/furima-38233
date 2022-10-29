5961bd3bf08fefe8fd419b08cb9e95466b792f1b
fumi23232323 <ftfumift@gmail.com>
2022-10-29 22:12:41 +0900

    削除機能の設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
-  before_action :set_item, only: [:show, :edit, :update]
+  before_action :set_item, only: [:show, :edit, :update, :destroy]
 
 
   def index
@@ -30,15 +30,25 @@ class ItemsController < ApplicationController
     end
   end
 
+  def destroy
+      if @item.user_id == current_user.id
+         @item.destroy
+         redirect_to root_path
+      else
+        redirect_to root_path
+      end
+     end
+
+end
   def update
     if @item.update(item_params)
       redirect_to item_path(@item.id)
     else
       render :edit
-    end
-  
+   end
   end
 
+    
   private
 
   def item_params
@@ -48,4 +58,4 @@ class ItemsController < ApplicationController
   def set_item
     @item = Item.find(params[:id])
   end
-end
+
